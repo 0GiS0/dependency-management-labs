1d40d04057b1ca0a43ec0aa5e0d3dd9dd048ef4e
0GiS0 <giselatb@outlook.com>
2023-10-12 18:59:50 +0000

    first commit

---
+++ steps.sh
@@ -0,0 +1,18 @@
+
+# Dependecy-track access: http://localhost:8080
+# Username: admin
+# Password: admin
+
+snyk auth
+
+git clone https://github.com/0GiS0/tour-of-heroes-angular.git && cd tour-of-heroes-angular
+npm install
+
+spdx-sbom-generator
+
+# snyk sbom --format cyclonedx1.4+json # Doesnt work
+
+git clone https://github.com/0GiS0/tour-of-heroes-dotnet-api.git &&  cd tour-of-heroes-dotnet-api
+dotnet build
+
+spdx-sbom-generator
